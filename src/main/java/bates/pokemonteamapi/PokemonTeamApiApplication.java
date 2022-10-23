973565b896a97d1e920a127cf6a6af66dce95b01
Greg Bates <gdbates1417@gmail.com>
2022-10-22 21:52:57 -0500

    Initial commit

---
+++ src/main/java/bates/pokemonteamapi/PokemonTeamApiApplication.java
@@ -0,0 +1,13 @@
+package bates.pokemonteamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTeamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTeamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bates/pokemonteamapi/PokemonTeamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bates.pokemonteamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTeamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
